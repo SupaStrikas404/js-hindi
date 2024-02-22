f83b0cd727729a7977248bf8e1db4508b0276cbe
Abhishek Raut <104448142+rautabhishek01@users.noreply.github.com>
2024-02-22 07:07:21 +0000

    new file add

---
+++ 01_basic/var.js
@@ -0,0 +1,12 @@
+const accId="KS124578"
+let accMail="kabir@gmail.com"
+var accName="Kabir"
+accPassword="54789"
+let accStatus
+
+// Try not to use "var" because of issue in scope
+/*
+console.log(accId);
+console.log(accName);
+*/
+console.table([accName, accId, accMail, accPassword, accStatus])
