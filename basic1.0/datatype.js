c55871092f7b3034505b318c2f2247db5318d90a
Abhishek Raut <104448142+rautabhishek01@users.noreply.github.com>
2024-02-22 08:59:05 +0000

    Datatype add

---
+++ basic1.0/datatype.js
@@ -0,0 +1,21 @@
+let fname="Kabir"
+let age=21
+let isGood=false
+let bankbal=null
+let gf
+
+/*
+string = ''  or ""
+number = 2 to power 53
+bigint (Use for huge numbers)
+boolean  =  true/ false
+object
+undefined = Not assigned
+symbol = Unique
+*/
+
+// console.log(typeof fname)
+// console.log(typeof age)
+// console.log(typeof isGood)
+console.log(typeof bankbal)       // object
+console.log(typeof gf)
