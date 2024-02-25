7f2be2692a7fbd5505c414a9c289426d35f51035
Abhishek Raut <104448142+rautabhishek01@users.noreply.github.com>
2024-02-25 16:42:49 +0000

    Conversion of Datatype

---
+++ basic1.0/condatatype.js
@@ -0,0 +1,10 @@
+let score=33
+let score1="33"
+let score2="23232nnn"
+console.log(typeof score)
+// console.log(typeof(score))
+// console.log(typeof score1)
+console.log(typeof(score1))
+let org=Number(score2)
+console.log(typeof org)
+console.log(org)
