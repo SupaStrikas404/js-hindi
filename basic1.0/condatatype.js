a2f2db844e0f24bd9a3828968a70f0afe3628356
Abhishek Raut <104448142+rautabhishek01@users.noreply.github.com>
2024-02-25 19:28:47 +0000

    Update of conversion

--- basic1.0/condatatype.js
+++ basic1.0/condatatype.js
@@ -1,10 +1,41 @@
+/*
 let score=33
-let score1="33"
-let score2="23232nnn"
+let score1="33"          
+
 console.log(typeof score)
-// console.log(typeof(score))
-// console.log(typeof score1)
 console.log(typeof(score1))
-let org=Number(score2)
+
+let score2="23232nnn"  
+let org=Number(score2)           //Type is number but Value is Not a Number
+
 console.log(typeof org)
 console.log(org)
+
+//NaN -> Not a Number
+
+// let a=true
+// let a=false
+let a=null
+// let a=undefined
+let aa=Number(a)
+console.log(aa)
+console.log(typeof aa)
+
+let a=2828
+let ab=String(a)
+console.log(ab)
+console.log(typeof ab)
+
+// let p=0
+// let p=1
+// let p="kabir"
+let p=""
+let pq=Boolean(p)
+console.log(pq)
+console.log(typeof pq)
+*/
+
+let no=22332
+let st=String(no)
+console.log(st)
+console.log(typeof st)
