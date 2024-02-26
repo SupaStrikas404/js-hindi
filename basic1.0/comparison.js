35617e78e58a400da613c4a4875ad2de886b6c9c
Abhishek Raut <104448142+rautabhishek01@users.noreply.github.com>
2024-02-26 17:06:30 +0000

    Name update

---
+++ basic1.0/comparison.js
@@ -0,0 +1,16 @@
+// console.log("2">1);
+// console.log("02"<1);
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null<=0);        // value conversion, null covert to a number. 
+// console.log(null>=0);       // value conversion, null covert to a number. 
+
+// console.log(undefined==0);
+// console.log(undefined>0);
+// console.log(undefined<=0);
+
+console.log("1"==1);
+console.log("1"===1);      // === -> strict check; cuz It checks the datatype
+
+// Equality check(==) and comparisons check(< > <= >=) work differently.
