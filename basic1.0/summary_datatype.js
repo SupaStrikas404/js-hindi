039c03655e3c7b4e07837d5a383181e6aa878ef5
Abhishek Raut <104448142+rautabhishek01@users.noreply.github.com>
2024-03-01 19:11:50 +0000

    type check

--- basic1.0/summary_datatype.js
+++ basic1.0/summary_datatype.js
@@ -1,6 +1,6 @@
 // Premitive Datatype
 // 7 Types: String, Number, Boolean, null, Symbol, undefined, BigInt.
-/*
+
 const mark=5657
 const markVal=278373.234
 const isjob=false
@@ -11,11 +11,11 @@ const want=undefined
 
 const id=Symbol('0005')
 const code=Symbol('0005')
-console.log(id===code);
+// console.log(id===code);
 
 const bigNumber=349843689345648689862396423n
-console.log(typeof bigNumber);
-*/
+// console.log(typeof bigNumber);
+
 
 // Non Premitive Datatype
 // Refference Type: Array, Objects, Function.
@@ -28,7 +28,11 @@ let myDet={
     money:null 
 }
 // console.log(myDet);
-const myFunc=function() {
+const myFunc = function() {
     console.log("Hi this is Kabir.");
 }
 console.log(myFunc);
+// console.log(typeof myFunc);
+// console.log(typeof myDet);
+// console.log(typeof frnd);
+// console.log(typeof bigNumber);
