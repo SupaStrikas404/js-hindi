f59117d157798aa1703c0a312158010d1d9fa95c
Abhishek Raut <104448142+rautabhishek01@users.noreply.github.com>
2024-03-01 17:09:52 +0000

    All about Datatype

---
+++ basic1.0/summary_datatype.js
@@ -0,0 +1,34 @@
+// Premitive Datatype
+// 7 Types: String, Number, Boolean, null, Symbol, undefined, BigInt.
+/*
+const mark=5657
+const markVal=278373.234
+const isjob=false
+const money=null
+const want=undefined
+// let password
+// console.log(typeof password);
+
+const id=Symbol('0005')
+const code=Symbol('0005')
+console.log(id===code);
+
+const bigNumber=349843689345648689862396423n
+console.log(typeof bigNumber);
+*/
+
+// Non Premitive Datatype
+// Refference Type: Array, Objects, Function.
+const frnd=["Ratul", "Souvik", "Sappy", "Avik"];
+// console.log(frnd);
+let myDet={
+    fname:"Kabir",
+    age:21,
+    isMarried: false,
+    money:null 
+}
+// console.log(myDet);
+const myFunc=function() {
+    console.log("Hi this is Kabir.");
+}
+console.log(myFunc);
