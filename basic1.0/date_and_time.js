62b826d0145566304cc115ec365f6a2122d43e0d
Abhishek Raut <104448142+rautabhishek01@users.noreply.github.com>
2024-03-08 08:57:47 +0000

    date and time com. code

--- basic1.0/date_and_time.js
+++ basic1.0/date_and_time.js
@@ -1,24 +1,35 @@
 // Dates 
 
-let myDate=new Date()
-console.log(myDate);      // Same
-  
+let myDate = new Date()
+console.log(myDate);                    // (Same)
+
+// console.log(`Today's day is ${myDate.toString()}}`);
+
+console.log(myDate.getMonth());        // Month counting start from '0'
+console.log(myDate.getDay());         // Day count start from 'Monday'
+
 // Method of Dates
 
-console.log(myDate.toLocaleString());
-console.log(myDate.toString());
-console.log(myDate.toLocaleDateString());
-console.log(myDate.toJSON());         // Same
-console.log(myDate.toDateString());
-console.log(myDate.toISOString());     // Same
-console.log(myDate.toTimeString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toJSON());                 // (Same)
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());           // (Same)
+// console.log(myDate.toTimeString());
 
 // console.log(typeof(myDate));
 
-// let myDay=new Date(2024, 2, 12)
-// console.log(myDay.toDateString());
+let myDay=new Date(2024, 2, 12)
+console.log(myDay.toDateString());
 
-const d = new Date(0);
+// const newDate = new Date(0);
 
-console.log(d.toString()); // "Thu Jan 01 1970 00:00:00 GMT+0000 (Coordinated Universal Time)"
-console.log(d.toTimeString()); // "00:00:00 GMT+0000 (Coordinated Universal Time)"
+// console.log(newDate.toString());             // "Thu Jan 01 1970 00:00:00 GMT+0000 (Coordinated Universal Time)"
+// console.log(newDate.toTimeString());        // "00:00:00 GMT+0000 (Coordinated Universal Time)"
+
+// Times
+
+let myTime=Date.now()
+console.log(myTime);                        // In Milisec.                      
+console.log(Math.ceil(myTime/1000));        // In Sec.
