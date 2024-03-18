602ec5516b02107305b62b938c6c42748c22d098
Abhishek Raut <104448142+rautabhishek01@users.noreply.github.com>
2024-03-18 07:38:28 +0000

    function start

---
+++ 3.0basic/function.js
@@ -0,0 +1,10 @@
+// A lot of code put inside the package and send the copy of it's and call that. 
+
+function myName() {
+    console.log("K");
+    console.log("a");
+    console.log("b");
+    console.log("i");
+    console.log("r");
+}
+myName
