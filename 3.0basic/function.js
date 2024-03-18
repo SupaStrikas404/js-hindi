9b30a1a3ad94f65fdc7ac832c3a408f9b3c4915c
Abhishek Raut <104448142+rautabhishek01@users.noreply.github.com>
2024-03-18 17:12:46 +0000

    function and parameters complete

--- 3.0basic/function.js
+++ 3.0basic/function.js
@@ -2,9 +2,30 @@
 
 function myName() {
     console.log("K");
-    console.log("a");
-    console.log("b");
-    console.log("i");
-    console.log("r");
+    console.log("A");
+    console.log("B");
+    console.log("I");
+    console.log("R");
 }
-myName
+// myName()
+
+// function muntiply(num1, num2) {
+//     console.log(num1 * num2);
+// }
+function muntiply(num1, num2) {
+    // let ans = (num1 * num2);
+    // return ans
+    return num1 * num2
+}
+const result = muntiply(3, 4)
+// console.log("Result :", result);
+
+function hack(userName) {
+    if (userName == undefined) {
+        console.log("Finding user's system...");
+        return
+    }
+    return `${userName} you system is hacked....`
+}
+console.log(hack());
+console.log(hack("Kabir"));
