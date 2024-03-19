b97c058879e4d6e63897eab8e1df5bc4877926d6
Abhishek Raut <104448142+rautabhishek01@users.noreply.github.com>
2024-03-19 03:07:32 +0000

    function all done

--- 3.0basic/function.js
+++ 3.0basic/function.js
@@ -1,4 +1,5 @@
 // A lot of code put inside the package and send the copy of it's and call that. 
+// Function and Parameters
 
 function myName() {
     console.log("K");
@@ -20,12 +21,48 @@ function muntiply(num1, num2) {
 const result = muntiply(3, 4)
 // console.log("Result :", result);
 
-function hack(userName) {
-    if (userName == undefined) {
+function hack(userName = "Syste's Name") {                      // Default user name is set.
+    if (!userName) {                                           // userName == undefined    OR    !userName
         console.log("Finding user's system...");
         return
     }
-    return `${userName} you system is hacked....`
+    return `${userName} your system is hacked....`
 }
-console.log(hack());
-console.log(hack("Kabir"));
+// console.log(hack());
+// console.log(hack("Kabir"));
+
+
+// Functions with Objects
+
+function calculateCartPrice(...item1Price) {
+    return item1Price
+}
+// function calculateCartPrice(item1, item2, ...item1Price) {
+//     return item1Price
+// }
+// console.log(calculateCartPrice(400, 200, 500, 1000));
+
+const user = {
+    userName: "Kabir",
+    location: "Durgapur",
+    isActive: true,
+    age: 21
+}
+function userDetails(anyUser) {
+    console.log(`This is ${anyUser.userName}, from ${anyUser.location}. His active status is ${anyUser.isActive} and age is ${anyUser.age}.`);
+}
+// userDetails(user)
+userDetails({
+    userName: "Ram",
+    location: "Ayodhya",
+    isActive: "true",
+    age: Infinity
+})
+
+const myArr = [100, 200, 300, 400, 500]
+
+function secondValue(anyArr) {
+    return anyArr[1]
+}
+// console.log(secondvalue(myArr));
+console.log(secondValue([300, 250, 100, 220]));
