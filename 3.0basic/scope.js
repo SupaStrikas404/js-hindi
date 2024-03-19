398d3299bfcc04a0bbf37b4071754a49b5dc7aa5
Abhishek Raut <104448142+rautabhishek01@users.noreply.github.com>
2024-03-19 08:19:26 +0000

    hoisting and scope complete

--- 3.0basic/scope.js
+++ 3.0basic/scope.js
@@ -49,7 +49,8 @@ function addone(num) {
     return num + 1
 }
 // (function with expression) also, known as hoisting
-console.log(addtwo(5));
+// console.log(addtwo(5));
 const addtwo = function (num) {
     return num + 2
 }
+console.log(addtwo(5));
