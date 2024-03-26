2a782b8d6c465a011271f45b314913f738fba5a3
Kabir <157941331+SupaStrikas404@users.noreply.github.com>
2024-03-26 15:53:49 +0000

    Operation done

--- 1.0basic/operation.js
+++ 1.0basic/operation.js
@@ -1,12 +1,12 @@
-console.log(2+3+"6");      //Though it's first variable is number datatype so, operation will happen.
-console.log("5"+2+1);     // Though it's first variable is string datatype so, operation will not happen cuz all variable will consider as string.
+console.log(2 + 3 + "6");      //Though it's first variable is number datatype so, operation will happen.
+console.log("5" + 2 + 1);     // Though it's first variable is string datatype so, operation will not happen cuz all variable will consider as string.
 
 console.log(+true);
 console.log(+"");
 
-let a=50
-let b=a++
-// let b=++a
+let a = 50
+// let b=a++
+let b = ++a
 console.log(b);
 console.log(a);
 
